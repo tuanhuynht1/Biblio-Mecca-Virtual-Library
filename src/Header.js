fc9cd7385f8376d89d063f5c00a07a4c248ef085
QIZHENG1998 <48461979+QIZHENG1998@users.noreply.github.com>
2020-04-12 11:37:58 -0400

    Delete Header.js

--- src/Header.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-
-// take in message as property, default message displays library name
-const Header = ({ message = "The Biblio Mecca" }) => {
-  return <h1>{message}</h1>;
-};
-
-export default Header;
