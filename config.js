d5d595bd4106b0d1d68bfb6398d8bcf41fa21682
QIZHENG1998 <48461979+QIZHENG1998@users.noreply.github.com>
2020-04-12 11:34:40 -0400

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// localhost:/[port]
-const port = 8000;
-// postgresql://[username]:[password]@localhost[postgres_port]/[database_name]
-const connectionString =
-  "postgresql://postgres:TT__tt7674@localhost:5432/postgres";
-
-export { port, connectionString };
